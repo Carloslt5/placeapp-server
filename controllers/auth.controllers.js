9916ba5023e6b498ea513f1ee31305d04bec7946
Carloslt <carloslt.proyectos@gmail.com>
2023-05-30 10:51:02 +0200

    controllers

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -3,37 +3,35 @@ const bcrypt = require('bcryptjs')
 const saltRounds = 10
 const User = require('../models/User.model')
 
-
-
 const signup = (req, res, next) => {
 
-    // const { path: avatar } = req.file
-    const { name, lastName, email, password } = req.body
+  // const { path: avatar } = req.file
+  const { name, lastName, email, password } = req.body
 
-    User
-        .findOne({ email })
-        .then((foundUser) => {
+  User
+    .findOne({ email })
+    .then((foundUser) => {
 
-            if (foundUser) {
-                res.status(400).json({ message: "User already exists." })
-                return
-            }
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." })
+        return
+      }
 
-            const salt = bcrypt.genSaltSync(saltRounds)
-            const hashedPassword = bcrypt.hashSync(password, salt)
+      const salt = bcrypt.genSaltSync(saltRounds)
+      const hashedPassword = bcrypt.hashSync(password, salt)
 
-            return User.create({ name, lastName, email, password: hashedPassword /*avatar: req.file?.path*/ })
-        })
-        .then((createdUser) => {
+      return User.create({ name, lastName, email, password: hashedPassword /*avatar: req.file?.path*/ })
+    })
+    .then((createdUser) => {
 
-            const { name, lastName, email, _id } = createdUser
-            const user = { name, lastName, email, _id }
+      const { name, lastName, email, _id } = createdUser
+      const user = { name, lastName, email, _id }
 
-            res.status(201).json({ user })
-        })
-        .catch(err => {
-            next(err)
-        })
+      res.status(201).json({ user })
+    })
+    .catch(err => {
+      next(err)
+    })
 }
 
 const login = (req, res, next) => {
@@ -61,12 +59,12 @@ const login = (req, res, next) => {
         const payload = { _id, email, name, lastName, avatar }
 
         const authToken = jwt.sign(
-          payload, 
-          process.env.TOKEN_SECRET, 
+          payload,
+          process.env.TOKEN_SECRET,
           { algorithm: 'HS256', expiresIn: "6h" }
         )
 
-        res.json({ authToken: authToken }); 
+        res.json({ authToken: authToken });
       }
       else {
         res.status(401).json({ message: "Unable to authenticate the user" });
@@ -79,13 +77,13 @@ const login = (req, res, next) => {
 
 const verify = (req, res, next) => {
 
-      res.status(200).json(req.payload)
+  res.status(200).json(req.payload)
 
 }
 
 module.exports = {
-    signup,
-    login,
-    verify
+  signup,
+  login,
+  verify
 
 }
