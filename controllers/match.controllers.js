30789ecfb8492013c332d115ae2f0d807161463e
Carloslt <carloslt.proyectos@gmail.com>
2023-06-07 00:30:02 +0200

    filter found

--- controllers/match.controllers.js
+++ controllers/match.controllers.js
@@ -5,13 +5,13 @@ const User = require('./../models/User.model')
 
 
 const getMatchPlaces = (req, res, next) => {
-    
-    const {profileId} = req.params
-     
-     const {_id : userId} = req.payload
-    
+
+    const { profileId } = req.params
+
+    const { _id: userId } = req.payload
+
     User
-        .find({ _id: { $in: [profileId, userId]}})
+        .find({ _id: { $in: [profileId, userId] } })
         .then((result) => {
 
             const arrOne = result[0].favouritePlaces.map(placeId => placeId.toString())
@@ -19,17 +19,20 @@ const getMatchPlaces = (req, res, next) => {
 
             const commonPlaces = arrOne.filter(placeId => arrTwo.includes(placeId))
 
+            const promises = commonPlaces.map(place => {
+                return Place.findById(place).then(result => result)
+            })
 
-            const promises = commonPlaces.map(place =>{
-               return Place.findById(place) .then(result => result) })
-            
             return Promise.all(promises)
-      
+
         })
-        .then((results) => res.json({results}))
+        .then((results) => {
+            console.log('SEGUNDO THENNN', results)
+            res.json({ results })
+        })
         .catch(err => next(err))
 
-res.json('ESTOY EN EL MATCH')
+    res.json('ESTOY EN EL MATCH')
 
 }
 
