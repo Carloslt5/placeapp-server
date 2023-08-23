0b87b59b8367934d5f3f9c0963f410d14a07fc76
Carloslt <carloslt.proyectos@gmail.com>
2023-08-23 11:43:38 +0200

    fix apikey

--- controllers/places.controllers.js
+++ controllers/places.controllers.js
@@ -8,214 +8,213 @@ const Comment = require('./../models/Comment.model')
 
 const getAllPlaces = (req, res, next) => {
 
-    Place
-        .find()
-        .sort({ name: 1 })
-        .then(foundPlaces => res.json(foundPlaces))
-        .catch(err => next(err))
+  Place
+    .find()
+    .sort({ name: 1 })
+    .then(foundPlaces => res.json(foundPlaces))
+    .catch(err => next(err))
 
 }
 
 
 const getUserPlaces = (req, res, next) => {
 
-    const { id } = req.params
+  const { id } = req.params
 
-    Place
-        .find({ owner: id })
-        .then(foundPlacesByUser => res.json(foundPlacesByUser))
-        .catch(err => next(err))
+  Place
+    .find({ owner: id })
+    .then(foundPlacesByUser => res.json(foundPlacesByUser))
+    .catch(err => next(err))
 
 }
 
 const getDetailsPlace = (req, res, next) => {
 
-    const { id } = req.params
+  const { id } = req.params
 
-    Place
-        .findById(id)
-        .populate("owner")
-        .populate("comments")
-        .then(foundPlace => res.json(foundPlace))
-        .catch(err => next(err))
+  Place
+    .findById(id)
+    .populate("owner")
+    .populate("comments")
+    .then(foundPlace => res.json(foundPlace))
+    .catch(err => next(err))
 
 }
 
 const getOnePlace = (req, res, next) => {
 
-    const { id } = req.params
+  const { id } = req.params
 
-    return placesApiHandler
-        .getDetailsPlace(id)
-        .then(({ data: { result } }) => result)
-        .then((placesDetailsResult) => {
+  return placesApiHandler
+    .getDetailsPlace(id)
+    .then(({ data: { result } }) => result)
+    .then((placesDetailsResult) => {
+      const {
+        place_id,
+        name,
+        editorial_summary,
+        photos,
+        international_phone_number,
+        current_opening_hours,
+        address_components,
+        formatted_address,
+        geometry
+      } = placesDetailsResult
 
-            const {
-                place_id,
-                name,
-                editorial_summary,
-                photos,
-                international_phone_number,
-                current_opening_hours,
-                address_components,
-                formatted_address,
-                geometry
-            } = placesDetailsResult
+      return photosPlacesApiHandler
+        .getPhotosPlaces(placesDetailsResult.photos[0].photo_reference)
+        .then((photoPlacesResult) => {
 
-            return photosPlacesApiHandler
-                .getPhotosPlaces(placesDetailsResult.photos[0].photo_reference)
-                .then((photoPlacesResult) => {
+          const urlPhotoResult = photoPlacesResult.request.res.responseUrl
 
-                    const urlPhotoResult = photoPlacesResult.request.res.responseUrl
+          const formattedPlace = {
+            placeId: place_id || 'data not available',
+            name: '',
+            description: editorial_summary?.overview || 'data not available',
+            placeImg: '',
+            photoReference: urlPhotoResult || 'data not available',
+            type: '',
+            phone: international_phone_number || 'data not available',
+            weekDay: current_opening_hours?.weekday_text || 'data not available',
+            city: address_components[2]?.long_name || 'data not available',
+            address: formatted_address || 'data not available',
+            latitude: geometry?.location.lat || 'data not available',
+            longitude: geometry?.location.lng || 'data not available',
+            userRating: '',
+            userOpinion: '',
+            owner: '',
+            comments: []
+          }
 
-                    const formattedPlace = {
-                        placeId: place_id || 'data not available',
-                        name: '',
-                        description: editorial_summary?.overview || 'data not available',
-                        placeImg: '',
-                        photoReference: urlPhotoResult || 'data not available',
-                        type: '',
-                        phone: international_phone_number || 'data not available',
-                        weekDay: current_opening_hours?.weekday_text || 'data not available',
-                        city: address_components[2]?.long_name || 'data not available',
-                        address: formatted_address || 'data not available',
-                        latitude: geometry?.location.lat || 'data not available',
-                        longitude: geometry?.location.lng || 'data not available',
-                        userRating: '',
-                        userOpinion: '',
-                        owner: '',
-                        comments: []
-                    }
-
-                    res.json(formattedPlace)
-
-                })
+          res.json(formattedPlace)
 
         })
-        .catch(err => next(err))
+
+    })
+    .catch(err => next(err))
 
 }
 
 const createPlace = (req, res, next) => {
 
-    const {
-        placeId,
-        name,
-        description,
-        placeImg,
-        photoReference,
-        type,
-        phone,
-        weekDay,
-        city,
-        address,
-        latitude,
-        longitude,
-        userRating,
-        userOpinion,
-        owner,
-        comments
-    } = req.body
+  const {
+    placeId,
+    name,
+    description,
+    placeImg,
+    photoReference,
+    type,
+    phone,
+    weekDay,
+    city,
+    address,
+    latitude,
+    longitude,
+    userRating,
+    userOpinion,
+    owner,
+    comments
+  } = req.body
 
-    const addressComponents = {
-        city: city,
-        address: address,
-        location: {
-            type: 'Point',
-            coordinates: [latitude, longitude]
-        }
+  const addressComponents = {
+    city: city,
+    address: address,
+    location: {
+      type: 'Point',
+      coordinates: [latitude, longitude]
     }
+  }
 
-    Place
-        .create({
-            placeId,
-            name,
-            description,
-            placeImg,
-            photoReference,
-            type,
-            phone,
-            weekDay,
-            addressComponents,
-            userRating,
-            userOpinion,
-            owner,
-            comments
-        })
-        .then(response => res.sendStatus(204))
-        .catch(err => next(err))
+  Place
+    .create({
+      placeId,
+      name,
+      description,
+      placeImg,
+      photoReference,
+      type,
+      phone,
+      weekDay,
+      addressComponents,
+      userRating,
+      userOpinion,
+      owner,
+      comments
+    })
+    .then(response => res.sendStatus(204))
+    .catch(err => next(err))
 
 }
 
 const editPlace = (req, res, next) => {
 
-    const { id } = req.params
-    const { type, userRating, userOpinion } = req.body
+  const { id } = req.params
+  const { type, userRating, userOpinion } = req.body
 
-    Place
-        .findByIdAndUpdate(id, { type, userRating, userOpinion }, { new: true })
-        .then(updatePlace => res.json(updatePlace))
-        .catch(err => next(err))
+  Place
+    .findByIdAndUpdate(id, { type, userRating, userOpinion }, { new: true })
+    .then(updatePlace => res.json(updatePlace))
+    .catch(err => next(err))
 
 }
 
 const addFavouritesPlace = (req, res, next) => {
 
-    const { id } = req.params
-    const { _id } = req.body
+  const { id } = req.params
+  const { _id } = req.body
 
-    User
-        .findByIdAndUpdate(_id, { $addToSet: { favouritePlaces: id } }, { new: true })
-        .then(() => res.sendStatus(204))
-        .catch(err => next(err))
+  User
+    .findByIdAndUpdate(_id, { $addToSet: { favouritePlaces: id } }, { new: true })
+    .then(() => res.sendStatus(204))
+    .catch(err => next(err))
 
 }
 
 const removefavouritesPlace = (req, res, next) => {
 
-    const { id } = req.params
-    const { _id } = req.body
+  const { id } = req.params
+  const { _id } = req.body
 
-    User
-        .findByIdAndUpdate(_id, { $pull: { favouritePlaces: id } }, { new: true })
-        .then(() => res.sendStatus(204))
-        .catch(err => next(err))
+  User
+    .findByIdAndUpdate(_id, { $pull: { favouritePlaces: id } }, { new: true })
+    .then(() => res.sendStatus(204))
+    .catch(err => next(err))
 
 }
 
 const deletePlace = (req, res, next) => {
 
-    const { id } = req.params
+  const { id } = req.params
 
-        Place
-        .findByIdAndDelete(id)
-        .then((deletePlace) => deletePlace)
-        .then((deletePlace) => {
+  Place
+    .findByIdAndDelete(id)
+    .then((deletePlace) => deletePlace)
+    .then((deletePlace) => {
 
-            deletePlace.comments.map((commentId)=> {
-               Comment
-                   .findByIdAndDelete(commentId)
-                   .then((result) => result)
-                   .catch(err => next(err))
-            })
-        })
-        .then((result) => res.sendStatus(204))
-        .catch(err => console.log(err))
+      deletePlace.comments.map((commentId) => {
+        Comment
+          .findByIdAndDelete(commentId)
+          .then((result) => result)
+          .catch(err => next(err))
+      })
+    })
+    .then((result) => res.sendStatus(204))
+    .catch(err => console.log(err))
 
-    
+
 
 
 }
 
 
 module.exports = {
-    getAllPlaces,
-    getUserPlaces,
-    getDetailsPlace,
-    createPlace,
-    getOnePlace,
-    editPlace,
-    addFavouritesPlace,
-    removefavouritesPlace,
-    deletePlace
+  getAllPlaces,
+  getUserPlaces,
+  getDetailsPlace,
+  createPlace,
+  getOnePlace,
+  editPlace,
+  addFavouritesPlace,
+  removefavouritesPlace,
+  deletePlace
 }
--- services/places.services.js
+++ services/places.services.js
@@ -1,16 +1,18 @@
 const axios = require('axios')
+const apiKey = process.env.GOOGLE_API_KEY
+
 
 class PlacesApiHandler {
 
-    constructor() {
-        this.axiosApp = axios.create({
-            baseURL: 'https://maps.googleapis.com/maps/api/place'
-        })
-    }
+  constructor() {
+    this.axiosApp = axios.create({
+      baseURL: 'https://maps.googleapis.com/maps/api/place'
+    })
+  }
 
-    getDetailsPlace(place_id) {
-        return this.axiosApp.get(`details/json?place_id=${place_id}&key=AIzaSyBIip4XUzH0gI5Hs2xkeNp4WCOsrjwHhpk`)
-    }
+  getDetailsPlace(place_id) {
+    return this.axiosApp.get(`details/json?place_id=${place_id}&key=${apiKey}`)
+  }
 
 }
 
